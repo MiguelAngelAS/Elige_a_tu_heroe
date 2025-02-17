98ce2f9bfc156ee56f31aa86480193525cdff221
MiguelAngelAS <miguelucho123456789gmail.com>
2025-02-16 20:25:48 -0500

    Se agrega el Proyecto

---
+++ MiguelA.js
@@ -0,0 +1,20 @@
+const images = ["Izuku-Midoriya-Transparent.png", "Kachan.png", "Shoto.png"];
+let currentIndex = 0;
+
+const imageDisplay = document.getElementById("imageDisplay");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+function updateImage() {
+    imageDisplay.src = images[currentIndex];
+}
+
+nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+});
+
+prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateImage();
+});
